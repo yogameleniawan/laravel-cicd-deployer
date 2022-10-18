5eeab6da3c7bacb299c8cdb1f1ba4f4ca17763a6
Yoga Meleniawan Pamungkas <yogameleniawan@gmail.com>
2022-10-18 13:20:14 +0700

    add deployer

---
+++ deploy.php
@@ -0,0 +1,78 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'contrib/npm.php';
+require 'contrib/rsync.php';
+require 'recipe/deploy/writable.php';
+
+set('bin/php', function () {
+    return '/usr/local/bin/php'; // change
+});
+
+set('application', 'Laravel CI/CD Deployer Github Workflow');
+set('repository', 'https://github.com/yogameleniawan/laravel-cicd-deployer.git');
+
+set('git_tty', true);
+set('git_ssh_command', 'ssh -o StrictHostKeyChecking=no');
+
+set('keep_releases', 5);
+
+set('writable_mode', 'chmod'); // shared hosting
+
+// Shared files/dirs between deploys
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
+
+// Writable dirs by web server
+add('writable_dirs', [
+    "bootstrap/cache",
+    "storage",
+    "storage/app",
+    "storage/framework",
+    "storage/logs",
+]);
+
+set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
+
+// Hosts
+
+host('DeployServer')
+->setHostname('151.106.119.33')
+->set('remote_user', 'u1318812')
+->set('port', 65002)
+->set('branch', 'master')
+->set('deploy_path', '~/public_html/api-deploy');
+
+// Tasks
+
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
+
+desc('Build assets');
+task('deploy:build', [
+    'npm:install',
+]);
+
+task('deploy', [
+    'deploy:prepare',
+    'rsync',                // Deploy code & built assets
+    'deploy:secrets',       // Deploy secrets
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:vendors',
+    'deploy:shared',
+    'artisan:storage:link',
+    'artisan:queue:restart',
+    'deploy:publish',
+    'deploy:unlock',
+]);
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release. Uncomment below code if you want to migrate after deploy
+
+before('deploy:symlink', 'artisan:migrate');
